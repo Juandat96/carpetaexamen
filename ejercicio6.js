c2d76efdb580c537d208f445ea3dfe2ecdc00eb9
Juandat96 <juandaniel171@gmail.com>
2016-06-14 21:17:56 -0500

    Add files via upload

---
+++ ejercicio6.js
@@ -0,0 +1,15 @@
+//ejercicio 6
+
+$(document).ready(function(){
+	$("#agregar").click(function(){
+		$("li").append("<li>")
+
+})
+
+$("borrar").click(function(){
+	$(this).prepend("<li>")
+
+
+
+})
+	});
